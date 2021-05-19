717df6e39883318cd15294d6647b79ddff324b04
Olfa <meksiolfa@gmail.com>
2021-05-19 16:36:00 +0200

    analyse survie 2/2 sur les 25 collect

--- A_survie/logrank_tte_collect.py
+++ A_survie/logrank_tte_collect.py
@@ -44,55 +44,45 @@ def event_duration(df):
     T,E = datetimes_to_durations(df["DDP"], df["DDCC"], freq="Y")
     df["event"] = E
 
-#fonction pour appliquer Kapplan_meier
-def categorical_km(feature, t='duration', event='event', df=df_all, ax=None):
-    df.loc[df.year_event.isna() == True, "duration"] = df.year_event.max() - df.year_event.min()
-    df.loc[df.year_event.isna() == False, "duration"]= df['year_event'] - df.year_event.min()  
-
-    T,E = datetimes_to_durations(df["DDP"], df["DDCC"], freq="Y")
-    df["event"] = E
-    for cat in df[feature].unique():
-        idx = df[feature] == cat
-        kmf = KaplanMeierFitter()
-
-        kmf.fit(df[idx][t], event_observed=df[idx][event], label=cat)    
-        kmf.plot(ax=ax, label=cat, ci_show=True, legend=False)
-    
-col_list = ["collectivite"]
-
-fig, axes = plt.subplots(nrows = 5, ncols = 5, 
-                         sharex = True, sharey = True,
-                         figsize=(30, 70)
-                        )
-
 collec = list(df_all.collectivite.unique())
 
-result = []
-for c1, c2 in itertools.combinations(collec, 2):
-    for ax in axes.flatten():    
-        df_2 = df_all.loc[(df_all.collectivite == c1) | (df_all.collectivite == c2)]
-        #event_duration(df_2)
-        categorical_km(feature="collectivite", t='duration', event='event', df = df_2, ax=ax)
-        p = multivariate_logrank_test(df_2['duration'], df_2["collectivite"], df_2['event'])
-        # ax.add_artist(AnchoredText(round(p.p_value, 2), frameon=False, 
-        #                            loc='lower right', prop=dict(size=46)))
-        if round(p.p_value, 2)>0.5 :
-            result.append(["collectivite", c1, c2, round(p.p_value, 2)])
-            categorical_km(feature="collectivite", t='duration', event='event', df = df_2, ax=ax)
-            ax.legend(loc='lower left', prop=dict(size=14))
-            ax.set_title("collectivite", pad=20, fontsize=56)
-            ax.set_xlabel('Duration', fontsize = 40)
-            ax.set_ylabel('Survival probability', fontsize = 40)
-            ax.grid()
-            ax.tick_params(axis='x', labelsize=30)
-            ax.tick_params(axis='y', labelsize=30)
-fig.tight_layout() 
+result_similar = []
+result_dispar = []
+for c1, c2 in itertools.combinations(collec, 2):   
+    df_2 = df_all.loc[(df_all.collectivite == c1) | (df_all.collectivite == c2)]
+    event_duration(df_2)
+    p = multivariate_logrank_test(df_2['duration'], df_2["collectivite"], df_2['event'])
+    if round(p.p_value, 2)>0.05 :
+        result_similar.append([ c1, c2, round(p.p_value, 2)])
+    if round(p.p_value, 2)<=0.05 :
+        result_dispar.append([c1, c2, round(p.p_value, 2)])
+        
+similar_data_collec = pd.DataFrame(result_similar, columns = ["Collectivite_1", "Collectivite_2", "p_value"])
+similar_data_collec.to_csv(PATH + 'similar_data_collect.csv')
+
+dispart_data_collec = pd.DataFrame(result_dispar, columns = ["Collectivite_1", "Collectivite_2", "p_value"])
+dispart_data_collec.to_csv(PATH + 'disparite_data_collect.csv')
+        
+        
 
 # for e in result:
 #     print("Catégorie: {} - Collectivités {} vs. {} : LOGRANK p value = {}".format(e[0], e[1],e[2],e[3] ))
     
 
 
+# #fonction pour appliquer Kapplan_meier
+# def categorical_km(feature, t='duration', event='event', df=df_all, ax=None):
+#     # df.loc[df.year_event.isna() == True, "duration"] = df.year_event.max() - df.year_event.min()
+#     # df.loc[df.year_event.isna() == False, "duration"]= df['year_event'] - df.year_event.min()  
+
+#     # T,E = datetimes_to_durations(df["DDP"], df["DDCC"], freq="Y")
+#     # df["event"] = E
+#     for cat in df[feature].unique():
+#         idx = df[feature] == cat
+#         kmf = KaplanMeierFitter()
+
+#         kmf.fit(df[idx][t], event_observed=df[idx][event], label=cat)    
+#         kmf.plot(ax=ax, label=cat, ci_show=True, legend=False)
 # fig, axes = plt.subplots(nrows = 1, ncols = 1, 
 #                          sharex = True, sharey = True,
 #                          figsize=(15, 10)
